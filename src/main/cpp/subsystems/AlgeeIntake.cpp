68b39f18420be64eab7d43b92f9e3d45fefef4ea
JCrobotics1 <johnsoncityrobotics@gmail.com>
2025-01-22 18:46:54 -0600

    V1.0.1 (B) added intake and outtake functions for the Algee intake

--- src/main/cpp/subsystems/AlgeeIntake.cpp
+++ src/main/cpp/subsystems/AlgeeIntake.cpp
@@ -3,12 +3,11 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/AlgeeIntake.h"
-using namespace rev::spark;
 
+using namespace rev::spark;
 
 AlgeeIntake::AlgeeIntake() = default;
 // AlgeeIntake::AlgeeIntake(const int Motor1CANID, const int algeeDetectionPort) : m_algeeIntakeMotorController() {}
-
 // This method will be called once per scheduler run
 void AlgeeIntake::Periodic() {}
 
@@ -17,9 +16,17 @@ void AlgeeIntake::IntakeAlgee() {
     // Probably looks something like checking the limmit swich
     // and setting the intake motor speed to pull in the algee
     // if it is NOT pressed
+    if (m_algeeDetectionLimmitSwich.Get() == false) {
+        m_algeeIntakeMotorController.Set(1.0);
+   
+    }
+    else{
+        m_algeeIntakeMotorController.StopMotor();
+    }
 }
 
 void AlgeeIntake::PlaceAlgee() {
     // This is much simpler. We probably just need to set the
     // motor speed to push the algee out.
+        m_algeeIntakeMotorController.Set(-1.0);
 }
