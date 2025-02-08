db3aebea9d41aad8969f418ecf87097babf8ba3f
QuaidThompson <quaidthompson2@gmail.com>
2025-02-08 11:22:01 -0600

    IntakeAlgae command template created

---
+++ src/main/cpp/commands/IntakeAlgae.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "IntakeAlgae.h"
+
+IntakeAlgae::IntakeAlgae() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void IntakeAlgae::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void IntakeAlgae::Execute() {}
+
+// Called once the command ends or is interrupted.
+void IntakeAlgae::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool IntakeAlgae::IsFinished() {
+  return false;
+}
