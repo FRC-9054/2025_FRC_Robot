99bc089aee014a454a6a201c7fe8d90a34fca5c7
QuaidThompson <quaidthompson2@gmail.com>
2025-02-08 11:34:12 -0600

    Fixed include error

--- src/main/cpp/commands/IntakeAlgae.cpp
+++ src/main/cpp/commands/IntakeAlgae.cpp
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "IntakeAlgae.h"
+#include "commands/IntakeAlgae.h"
 
 IntakeAlgae::IntakeAlgae() {
   // Use addRequirements() here to declare subsystem dependencies.
