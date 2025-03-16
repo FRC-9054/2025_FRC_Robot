25666496895875e5df8bb5deffffed7c2cf07ebb
QuaidThompson <quaidthompson2@gmail.com>
2025-03-15 20:53:44 -0500

    Swapped to an xbox remote for relyability

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -37,8 +37,8 @@ constexpr double kMagnitudeSlewRate =
 constexpr double kRotationalSlewRate =
     0.8;  // percent per second (1 = 100%)    // 2.0
     
-constexpr double kxInputCurveFactor = 1;
-constexpr double kyInputCurveFactor = 1;
+constexpr double kxInputCurveFactor = 1.8;
+constexpr double kyInputCurveFactor = 1.8;
 constexpr double krotInputCurveFactor = 1;
 
 // Chassis configuration
@@ -146,7 +146,7 @@ const int winchMotorCANID = 20;
 const int algeeDetectionLimmitSwich1Port = 1;
 const int algeeDetectionLimmitSwich2Port = 2;
 const int intakeSetDetectionLimmitSwichPort = 3;
-const float IntakeSpeed = 0.2;    // 0.3
+const float IntakeSpeed = 0.25;    // 0.3
 const float OuttakeSpeed = 0.15;  // -.3
 const float WinchSpeedPull = 0.5f;
 const float WinchSpeedPush = -0.5f;
